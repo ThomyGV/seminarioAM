22db7fd28fd8c894b6abd8f61c4b7955ffdc81ed
HungusBogassi <albertopaladea@gmail.com>
2023-09-04 23:29:43 -0300

    modificacion 04-09

---
+++ Dune/app/src/main/java/com/example/dune/SplashScreenActivity.kt
@@ -0,0 +1,25 @@
+package com.example.dune
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.activity.ComponentActivity
+
+class SplashScreenActivity : ComponentActivity() {
+
+    private val demora : Long 2000
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+
+        val r = Runnable {
+            intento = Intent(this, MainActivity::class.java)
+            startActivity(intento)
+            finish();
+        }
+        Handler(Looper.getMainLooper()).postDelayed(r, demora)
+
+    }
+}
