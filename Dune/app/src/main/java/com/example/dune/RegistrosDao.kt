55b4c079625ec54ffb1dafbb8a5344d5292a1136
HungusBogassi <albertopaladea@gmail.com>
2023-09-10 20:25:07 -0300

    modificacion 10-09

--- Dune/app/src/main/java/com/example/dune/RegistrosDao.kt
+++ Dune/app/src/main/java/com/example/dune/RegistrosDao.kt
@@ -9,6 +9,9 @@ interface RegistrosDao {
     @Query("select * from Registros")//aca uso la clase como nombre de la tabla
     fun getAll(): List<Registros>
 
+    @Query("select usuario from Registros")//aca uso la clase como nombre de la tabla
+    fun getUsuario(usuario : String): Boolean
+
     @Insert
     fun insertRegistro(registro: Registros)
 }
